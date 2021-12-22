42e9ce171c87824ff8107a5b948dae78811e72d5
Phil Johnson <phytoporg@gmail.com>
2021-12-22 14:26:43 -0800

    Moving test scripts to new scripts directory

---
+++ scripts/run_client.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/bash
+
+# Keepin' arguments simple for now
+SONGS_DIR="$1"
+if [ -z "$SONGS_DIR" ]; then
+    echo "Please supply a songs directory argument."
+    exit -1
+fi
+
+SERVER_HOST="$2"
+if [ -z "$SERVER_HOST" ]; then
+    echo "No server host provided, defaulting to localhost"
+    SERVER_HOST="localhost"
+fi
+
+SERVER_PORT="$3"
+if [ -z "$SERVER_PORT" ]; then
+    echo "No server port provided, defaulting to 8000"
+    SERVER_PORT=8000
+fi
+
+cd "$(dirname "$0")"
+python client.py --songs-dir "$SONGS_DIR" --server-host "$SERVER_HOST" --server-port "$SERVER_PORT"
+cd -
